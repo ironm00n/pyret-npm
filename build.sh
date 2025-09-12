9bef01605bf3b88f7ee1cc2bc286df386c173257
IRONM00N <me@ironmoon.dev>
2025-09-12 10:54:38 -0400

    fork

--- build.sh
+++ build.sh
@@ -1,11 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 rm -rf pyret-lang
-git clone --single-branch -b horizon https://github.com/brownplt/pyret-lang.git
+git clone --depth 1 --single-branch -b fork https://github.com/ironm00n/pyret-lang.git
 
 pushd pyret-lang
-npm install
+
+
+# npm publish (and npm pack) run all lifecycle scripts in "dry-run" mode.
+# If we don’t override that flag, nested npm command only *pretend* to
+# install: they print “added ... packages” but write no node_modules tree, so
+# later steps (like browserify) fail. We therefore force a real install
+# by passing --dry-run=false.
+npm ci --dry-run=false
+
 make phaseA libA
 touch .npmignore
 popd
